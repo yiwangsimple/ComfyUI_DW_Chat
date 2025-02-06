cfdaae8ab557ecb824ca8c27e7267c51ca17c6c8
yunxiang wei <121092793+yiwangsimple@users.noreply.github.com>
2025-02-06 11:34:59 +0800

    新增 deepseek-r1-distill-llama-70b 模型的支持

--- nodes/groqchat.py
+++ nodes/groqchat.py
@@ -27,7 +27,7 @@ class GroqChatNode:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "model": (["gemma-7b-it", "gemma2-9b-it", "mixtral-8x7b-32768", "llama3-8b-8192", "llama3-70b-8192","llama3-groq-8b-8192-tool-use-preview","llama3-groq-70b-8192-tool-use-preview", "llama-3.1-8b-instant", "llama-3.1-70b-versatile", "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-7b-preview", "llama-3.2-11b-vision-preview", "llama-3.2-90b-vision-preview", "llava-v1.5-7b-4096-preview", "llama-guard-3-8b"],),
+                "model": (["deepseek-r1-distill-llama-70b", "gemma-7b-it", "gemma2-9b-it", "mixtral-8x7b-32768", "llama3-8b-8192", "llama3-70b-8192","llama3-groq-8b-8192-tool-use-preview","llama3-groq-70b-8192-tool-use-preview", "llama-3.1-8b-instant", "llama-3.1-70b-versatile", "llama-3.2-1b-preview", "llama-3.2-3b-preview", "llama-3.2-7b-preview", "llama-3.2-11b-vision-preview", "llama-3.2-90b-vision-preview", "llava-v1.5-7b-4096-preview", "llama-guard-3-8b"],),
                 "prompt": ("STRING", {"multiline": True}),
                 "max_tokens": ("INT", {"default": 1000, "min": 1, "max": 32768}),
                 "temperature": ("FLOAT", {"default": 0.7, "min": 0, "max": 2, "step": 0.1}),
